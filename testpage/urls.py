ad4a074751271dfd20e084b4a35b91a23df40160
marlowequart <mqdataw@gmail.com>
2018-08-07 13:58:02 -0700

    updating files to direct index to script

--- testpage/urls.py
+++ testpage/urls.py
@@ -16,7 +16,25 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+#from django.template.loader import render_to_string
+#from django.http import HttpResponse
+
+# def home(request):
+	# title= 'home page'
+	# html = render_to_string('home.html', {'title': title})
+	# return HttpResponse(html)
+	
+	
+	
+# def about(request):
+	# title= 'about page'
+	# html = render_to_string('about.html', {'title': title})
+	# return HttpResponse(html)
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'^about/$',about),
+#    url(r'^$', TemplateView.as_view(template_name='home.html'), name='homepage'),
+	url(r'^index.+?$', TemplateView.as_view(template_name='index.html'), name='homepage'),
+#	url(r'^admin/', admin.site.urls),
+	url(r'^about/$',TemplateView.as_view(template_name='about.html'), name='aboutpage'),
 ]
--- testpage/wsgi.py
+++ testpage/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for marketsearch project.
+WSGI config for testpage project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marketsearch.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testpage.settings")
 
 application = get_wsgi_application()
