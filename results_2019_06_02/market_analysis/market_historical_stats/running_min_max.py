44e0857278a4d28f3e6f68f9b1afa4e81ab69a40
Marlowe Quart <Marlowe@marlowe.local>
2019-06-03 00:35:12 -0700

    updates

---
+++ results_2019_06_02/market_analysis/market_historical_stats/running_min_max.py
@@ -0,0 +1,46 @@
+'''
+file name: running_min_max.py
+date: June 2, 2019
+description: Class to store running minimum and maximum
+
+
+inputs:
+	__init__:
+		data_point, line 27
+	reset:
+		data_point, line 31
+	update:
+		data_point, line 36
+outputs:
+	__init__:
+		running_min_max object, line 16-17
+	reset:
+		Nothing, edits object properties
+	update:
+		Nothing, edits object properties
+
+Python version 3.6.7, modules used: datetime.
+'''
+
+class running_min_max():
+	""" Class to store running minimum and maximum for the market data """
+	def __init__(self, data_point:"[date as datetime.datetime, price as float]"):
+		""" Set both min and max to the current data point """
+		self.reset(data_point)
+
+	def reset(self, data_point:"[date as datetime.datetime, price as float]") -> None:
+		""" Reset both min and max to the current data point """
+		self.minimum = data_point.copy()
+		self.maximum = data_point.copy()
+
+	def update(self, data_point:"[date as datetime.datetime, price as float]") -> None:
+		""" Update min and max if necessary """
+		# if current price is more then running max
+		#	 then update running maximum
+		if data_point[1] > self.maximum[1]: 
+			self.maximum = data_point.copy()
+		# if current price is less than running min
+		#	 then update running minimum
+		if data_point[1] < self.minimum[1]: 
+			self.minimum = data_point.copy()
+
---
+++ results_2019_06_02/results_plotter.py
@@ -0,0 +1,194 @@
+# 6/2/19
+# load data from csv
+# Plot
+
+
+# ~ import pprint
+# ~ import copy
+
+# ~ import csv
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+
+def import_data(file_name):
+	#only add items in fields list to dataframe
+	
+	#only add date, open, high, low, last to dataframe
+	# fields = ['Date','Open','High','Low','Close']
+	
+	#open the file using pandas, use the first row as the header
+	# ~ data = pd.read_csv(file_name,header=0,usecols=fields)
+	data = pd.read_csv(file_name,header=0)
+	
+	#change the order to most recent date on top if necessary
+	data=data.sort('Date',ascending=0)
+	data=data.reset_index(drop=True)
+	# ~ print(data.head(5))
+	
+	return data
+
+
+def main():
+	####
+	# Open results datafile
+	####
+
+	#####
+	# input main info here
+	#####
+
+	# Data location for mac:
+	# path = '/Users/Marlowe/Marlowe/Securities_Trading/_Ideas/Data/'
+	path = '/Users/Marlowe/gitsite/transfer/results_2019_06_02/'
+
+	#input file name of interest
+	in_file_name='RUT_output.csv'
+
+	in_file= os.path.join(path,in_file_name)
+
+
+	# create dataframe from the data
+	df=import_data(in_file)
+
+	# ~ print(df.head())
+	# ~ return
+
+	# Go through df to output data in the following format:
+
+	#	return {"bull_markets":bulls, "bear_markets":bears},
+	#		type dict: {"bull_markets" : bull markets as [[date as datetime.datetime, closing price as float], ...], 
+	#		"bear_markets": bear markets as [[date as datetime.datetime, closing price as float], ...]}
+
+	data_dict={
+				'undefined': [],
+				'Bear': [],
+				'Bull': [],
+				'Bull Correction': [],
+				'Bear Correction': [],
+				'Bull Pullback': [],
+				'Bear Pullback': []
+	}
+	
+	for idx in range(len(df)-10,len(df)):
+		curr_mkt=df['Market'][idx]
+		curr_close=float(df['Close'][idx])
+		curr_date=datetime.strptime(df['Date'][idx],"%Y-%m-%d")
+		
+		curr_add=[curr_date,curr_close]
+		
+		data_dict[curr_mkt].append(curr_add)
+		
+	# ~ print(data_dict)
+	# ~ return
+	
+	####
+	# PLOT Data
+	####
+	#plot with line overlays
+	'''
+	plt.figure()
+	plt.title("Markets, bull corrections and bull pullbacks on ^GSPC")
+	plt.yscale('log')
+
+	plt.plot(*lTranspose(sampleDataset))
+
+	def plotMarket(market, color, label):
+		for i,_ in enumerate(market): # PLOT BULL MARKETS
+			if i==0: 
+				plt.plot(*lTranspose(_), color=color, label=label)
+			else: 
+				plt.plot(*lTranspose(_), color=color)
+
+	plotMarket(allBulls, 'green', 'bull market')
+	plotMarket(allBears, 'red', 'bear market')
+	plotMarket(flattenList(allBullCorrs), 'orange', 'bull correction')
+	plotMarket(flattenList(allBullPullbacks), 'yellow', 'bull pullback')
+	plotMarket(flattenList(allBearCorrs), 'orange', 'bull correction')
+	plotMarket(flattenList(allBearPullbacks), 'yellow', 'bull pullback')
+	   
+	plt.legend()
+	plt.show()
+	'''
+
+
+	'''
+	print("Latest price is %s" %sampleDataset[-1])
+	if allBulls[-1][1]<allBears[-1][1]: 
+		print("Currently we are in a bull market, \nlast bear market was %s"  %allBears[-1])
+		marketAge=sampleDataset[-1][0]-allBears[-1][1][0]
+	else:
+		print("Currently we are in a bear market, \nlast bull market was %s"  %allBulls[-1])
+		marketAge=sampleDataset[-1][0]-allBulls[-1][1][0]
+	print("Market age is %s" %marketAge)
+	'''
+
+	#plot with markers
+	import copy
+	sampleDatasetEdit=copy.deepcopy(sampleDataset) # we wouldn't want to mess up our data
+
+	for dp in sampleDatasetEdit:
+		dp.append('undefined')# add a third column to store market type
+		dp.append('undefined')# add a forth column to store corrections
+		dp.append('undefined')# add a fifth column to store pullbacks
+
+	# not a computationally efficient solution, but quick to implement
+	def updateColumnTo(dset,states,columnNum,value):
+		for dpoint in dset:
+			if dpoint[0]==states[0][0] or dpoint[0]==states[1][0]:
+				dpoint[columnNum]='undefined'
+			if dpoint[0]>states[0][0] and dpoint[0]<states[1][0]:
+				dpoint[columnNum]=value
+
+
+	for bull in allBulls:
+		updateColumnTo(sampleDatasetEdit,bull,2,'bull')
+	for bear in allBears:
+		updateColumnTo(sampleDatasetEdit,bear,2,'bear')
+		
+	for _ in flattenList(allBullCorrs):
+		updateColumnTo(sampleDatasetEdit,_,3,'correction')
+	for _ in flattenList(allBullBwCorrs):
+		updateColumnTo(sampleDatasetEdit,_,3,'between_corrections')
+	for _ in flattenList(allBearCorrs):
+		updateColumnTo(sampleDatasetEdit,_,3,'correction')
+	for _ in flattenList(allBullBwCorrs):
+		updateColumnTo(sampleDatasetEdit,_,3,'between_corrections')
+			
+	for _ in flattenList(allBullPullbacks):
+		updateColumnTo(sampleDatasetEdit,_,4,'pullback')
+	for _ in flattenList(allBullBwPullbacks):
+		updateColumnTo(sampleDatasetEdit,_,4,'between_pullbacks')
+	for _ in flattenList(allBearPullbacks):
+		updateColumnTo(sampleDatasetEdit,_,4,'pullback')
+	for _ in flattenList(allBullBwPullbacks):
+		updateColumnTo(sampleDatasetEdit,_,4,'between_pullbacks')
+
+	#set plot start and finish
+	p_start=0
+	p_finish=7823
+	# ~ print(len(sampleDatasetEdit))
+
+	test_bull_bw_pullbacks=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bull' and _[4]=='between_pullbacks']
+	test_bull_corrs=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bull' and _[3]=='correction']
+	test_bull_pullbacks=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bull' and _[4]=='pullback']
+	test_bear=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bear']
+	test_bear_corrs=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bear' and _[3]=='correction']
+	test_bear_pullbacks=[_[0:2] for _ in sampleDatasetEdit[p_start:p_finish] if _[2]=='bear' and _[4]=='pullback']
+
+	# ~ plt.figure(figsize=(20,8))
+	plt.figure()
+	plt.title("Markets, bull corrections and bull pullbacks on ^GSPC")
+	plt.yscale('log')
+	plt.plot(*lTranspose(test_bull_bw_pullbacks),'.', markersize=1, color='green')
+	plt.plot(*lTranspose(test_bear),'.', markersize=1, color='red')
+	plt.plot(*lTranspose(test_bull_corrs),'.', markersize=1, color='orange')
+	plt.plot(*lTranspose(test_bull_pullbacks),'.', markersize=1, color='yellow')
+	plt.plot(*lTranspose(test_bear_corrs),'.', markersize=1, color='orange')
+	plt.plot(*lTranspose(test_bear_pullbacks),'.', markersize=1, color='yellow')
+	plt.show()
+
+
+main()
