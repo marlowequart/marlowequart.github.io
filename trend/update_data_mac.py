d59b6af34520d2546b3cc49e662cd368f901fccb
Marlowe Quart <Marlowe@marlowe.local>
2020-03-04 06:44:44 -0800

    udpates

--- trend/update_data_mac.py
+++ trend/update_data_mac.py
@@ -30,27 +30,102 @@ math, # built-in, Python 3.5.5
 # import numpy as np
 # import math
 # import statistics
-# import datetime
+import datetime
 import time
+import glob
+import pyautogui as pg
+import re
 
-# ~ import pyautogui as pg
 
+def get_filename_and_date(filename):
+	#input a file name string date in format SYM_YYYY_MM_DD.csv
+	#output symbol, year, month, day
+	new_name=re.sub('.csv','',filename)
+	# ~ print(new_name)
+	
+	# get everything up to the first underscore
+	sym=re.search('^[^_]+(?=_)',new_name)
+	symbol=sym.group(0)
+	# ~ print(symbol)
+	
+	# get everything after the first underscore
+	regexp=re.compile('_(.*)$')
+	full_date=regexp.search(new_name).group(1)
+	# ~ print(full_date)
+	
+	# get everything up to the first underscore
+	yr=re.search('^[^_]+(?=_)',full_date)
+	year=int(yr.group(0))
+	# ~ print(year)
+	
+	# get everything after the first underscore
+	mo_day_date=regexp.search(full_date).group(1)
+	mo=re.search('^[^_]+(?=_)',mo_day_date)
+	month=int(mo.group(0))
+	# ~ print(month)
+	
+	# get everything after the last underscore
+	day_date=re.search('(?<=_).*',mo_day_date)
+	day=int(day_date.group(0))
+	# ~ print(day)
+	
+	return symbol,year,month,day
 
+def get_file_list(directory):
+	# get the list of files in the directory
+	file_list=glob.glob(directory+'*.csv')
+	# ~ print(file_list)
+	
+	files=[]
+	for file_name in file_list:
+		files.append(re.sub(directory,'',file_name))
+	
+	return files
 
+def create_dict(list_of_files):
+	# create a dictionary with symbol:[date,file_name]
+	out_dict={}
+	
+	for item in list_of_files:
+		sym,yr,mo,day=get_filename_and_date(item)
+		out_dict[sym]=[datetime.datetime(yr,mo,day),item]
+	
+	return out_dict
 
 
 def main():
 	# wait 5 seconds before beginning
-	time.sleep(10)
+	# ~ time.sleep(1)
 	
 	current_working_dir='/Users/Marlowe/gitsite/transfer/trend/data/working_dir/'
 	download_data_dir='/Users/Marlowe/gitsite/transfer/trend/ts_data/'
 	
-	# Get list of file names in the download_data_dir
+	# Open a new terminal window
+	# ~ pg.hotkey('command','n')
+	# ~ time.sleep(1)
 	
-	# Remove the date from the file names
+	# update folders from github
+	# ~ pg.write('git pull origin master',interval=0.05)
+	# ~ pg.press('enter')
+	# ~ time.sleep(10)
+	
+	# ~ # Navigate to the download dir
+	# ~ pg.write('cd '+download_data_dir,interval=0.05)
+	# ~ pg.press('enter')
+	# ~ time.sleep(2)
+	
+	# Get list of file names in the download_data_dir
+	# create a dictionary of files with symbol:download date
+	new_files=get_file_list(download_data_dir)
+	new_files_dict=create_dict(new_files)
+	
+	# Get list of file names in the current_working_dir
+	# create a dictionary of files with symbol:download date
+	old_files=get_file_list(current_working_dir)
+	old_files_dict=create_dict(old_files)
 	
 	# check for duplicate files in the current working dir
+	
 	
 	# remove the duplicates from the current working dir
 	
